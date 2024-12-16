1b5fcd175dd12057d2049ce208b6dc28f719540c
Богачева Анастасия <britta18britta@gmail.com>
2024-12-16 22:25:23 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,11 @@
+immutable_var = 1,2,3,4,'hello',True
+print(immutable_var)
+
+#immutable_var[4] = 'good day' кортеж не поддерживает обращение по элементам.
+# в кортеже нельзя ничего поменять
+
+mutable_list = ['Добрый день,', 'Анастасия!']
+mutable_list[1] = 'Валерия'
+print(mutable_list)
+
+
